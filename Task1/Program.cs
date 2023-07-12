8c40e3f6a0c399c9ada62f0cc55124f43a07ce50
Kseniya <Ksyu.shapovalenko@gmail.com>
2023-07-12 22:08:59 +0300

    Добавили папку с задачей

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double max;
+double min;
+ if(a>b)
+ {
+    max=a;
+    min=b;
+ }
+else {
+    max=b;
+    min=a;
+    }
+if (min==max) { Console.WriteLine("Данные числа равные"); }
+else {Console.WriteLine("Большее число: " + max + ". Меньшее число: " + min);}
