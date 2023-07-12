d6333888f546b3e8d3ab01659ac76ebf9b1826cb
Kseniya <Ksyu.shapovalenko@gmail.com>
2023-07-12 23:06:51 +0300

    Написана задача на нахождение максимального из трех чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double max=a;
+
+if (max<b)
+{
+    max=b;
+}
+
+if (max<c)
+{
+    max=c;
+}
+
+Console.WriteLine($"Максимальное из трёх чисел: {max}");
