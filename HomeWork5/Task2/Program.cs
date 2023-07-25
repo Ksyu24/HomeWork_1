08198c78155df4e69d4e678ca151c2e46b8aaabf
Kseniya <Ksyu.shapovalenko@gmail.com>
2023-07-25 13:43:26 +0300

    Решена 2 задача

--- HomeWork5/Task2/Program.cs
+++ HomeWork5/Task2/Program.cs
@@ -1,38 +1,41 @@
-﻿Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-[3, 7, 23, 12] -> 19
-[-4, -6, 89, 6] -> 0
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
-int array = new int[10];
+int[] array = new int[10];
 FillingArray(array);
-ConsoleWriteLine(OutputArray(array)+"  ->  "+FindSumElementsInOddPos(array));
+Console.WriteLine(OutputArray(array) + "  ->  " + FindSumElementsInOddPos(array));
 
 void FillingArray(int[] arr)
 {
-	Random rnd=new Random();
-	for(int i=0; i<arr.Length; i++)
-	{
-		arr[i]=rnd.Next();
-	}
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-999, 1000);
+    }
 }
 
 string OutputArray(int[] arr)
 {
-	string result="["+arr[0];
-	for(int i=1; i<arr.Length; i++)
-	{
-		result=result+$", {arr[i]}");
-	}
-	result=esult+"]";
-	return result;
+    string result = "[" + arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        result = result + $", {arr[i]}";
+    }
+    result = result + "]";
+    return result;
 }
 
 int FindSumElementsInOddPos(int[] arr)
 {
-	int sum=0;
-	for(int i=1; i<arr.Length; i+2)
-	{
-		sum=sum+arr[i];
-	}
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum = sum + arr[i];
+        }
+    }
 
-	return sum;
+    return sum;
 }
