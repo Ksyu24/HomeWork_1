463a52d71be6650c48106ee99fc684022c3906d9
Kseniya <Ksyu.shapovalenko@gmail.com>
2023-07-25 13:28:29 +0300

    Отформатирована 1 задача

--- HomeWork5/Task1/Program.cs
+++ HomeWork5/Task1/Program.cs
@@ -3,35 +3,37 @@
 
 int[] array = new int[10];
 FillingArray(array);
-Console.WriteLine(OutputArray(array)+"  ->  "+FindCountEvenNum(array));
+Console.WriteLine(OutputArray(array) + "  ->  " + FindCountEvenNum(array));
 
 void FillingArray(int[] arr)
 {
-	Random rnd=new Random();
-	for(int i=0; i<arr.Length; i++)
-	{
-		arr[i]=rnd.Next(100,1000);
-	}
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
 }
 
 string OutputArray(int[] arr)
 {
-	string result="["+arr[0];
-	for(int i=1; i<arr.Length; i++)
-	{
-		result=result+$", {arr[i]}";
-	}
-	result=result+"]";
-	return result;
+    string result = "[" + arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        result = result + $", {arr[i]}";
+    }
+    result = result + "]";
+    return result;
 }
 
 int FindCountEvenNum(int[] arr)
 {
-	int count=0;
-	for(int i=0; i<arr.Length; i++)
-	{
-		if(arr[i]%2==0)
-		{count++;}
-	}
-	return count;
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
 }
---
+++ HomeWork5/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+
+int array = new int[10];
+FillingArray(array);
+ConsoleWriteLine(OutputArray(array)+"  ->  "+FindSumElementsInOddPos(array));
+
+void FillingArray(int[] arr)
+{
+	Random rnd=new Random();
+	for(int i=0; i<arr.Length; i++)
+	{
+		arr[i]=rnd.Next();
+	}
+}
+
+string OutputArray(int[] arr)
+{
+	string result="["+arr[0];
+	for(int i=1; i<arr.Length; i++)
+	{
+		result=result+$", {arr[i]}");
+	}
+	result=esult+"]";
+	return result;
+}
+
+int FindSumElementsInOddPos(int[] arr)
+{
+	int sum=0;
+	for(int i=1; i<arr.Length; i+2)
+	{
+		sum=sum+arr[i];
+	}
+
+	return sum;
+}
