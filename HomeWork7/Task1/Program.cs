4ad0ba5c9d7c492a512be100e016200631f08a6f
Kseniya <Ksyu.shapovalenko@gmail.com>
2023-08-08 15:29:00 +0300

    Решена 1 задача

---
+++ HomeWork7/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+double [,] array = new double[3,4]; //[строки, столбцы]
+FillArray(array);
+PrintArray(array);
+
+void FillArray(double[,] arr)
+{
+	Random rnd=new Random();
+	for (int i=0; i<arr.GetLength(0); i++)
+	{
+		for (int j=0; j<arr.GetLength(1); j++)
+		{
+			arr[i,j]=Math.Round(rnd.NextDouble()*(10+10)-10, 2);
+		}
+	}
+
+}
+
+void PrintArray(double[,] arr)
+{
+	
+	for(int i=0; i<arr.GetLength(0); i++)
+	{
+		for(int j=0; j<arr.GetLength(1); j++)
+		{
+			Console.Write($"{arr[i,j]}\t");
+		}
+		Console.WriteLine();
+	}
+}
---
+++ HomeWork8/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Двумерные массивы. Продолжение
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] array=new int[3, 3];
+FillingArray(array);
+PrintArray(array);
+
+
+void FillingArray(int[,] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            arr[i, j] = rnd.Next(1, 15);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    
+}
