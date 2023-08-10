f0d617573dcb69b67a51611cceae2aec2973dde2
Kseniya <Ksyu.shapovalenko@gmail.com>
2023-08-10 22:32:08 +0300

    Решена 4 задача

--- HomeWork8/Task3/Program.cs
+++ HomeWork8/Task3/Program.cs
@@ -6,8 +6,8 @@
 // 18 20
 // 15 18
 
-int[,] array1 = new int[2,2];
-int[,] array2 = new int[2,2];
+int[,] array1 = new int[2, 2];
+int[,] array2 = new int[2, 2];
 FillingArray(array1);
 FillingArray(array2);
 
@@ -18,61 +18,61 @@ Console.WriteLine("Вторая матрица (множитель):");
 PrintArray(array2);
 Console.WriteLine();
 
-if(array1.GetLength(1)==array2.GetLength(0))
+if (array1.GetLength(1) == array2.GetLength(0))
 {
-	int[,]	array=MultiplieArray(array1, array2);
-	Console.WriteLine("Результирующая матрица (произведение):");
-	PrintArray(array);
+    int[,] array = MultiplieArray(array1, array2);
+    Console.WriteLine("Результирующая матрица (произведение):");
+    PrintArray(array);
 }
-else{Console.WriteLine("Невозможно найти произведение данных матриц, из-за их размерности!");}
+else { Console.WriteLine("Невозможно найти произведение данных матриц, из-за их размерности!"); }
 
 
 
 void FillingArray(int[,] arr)
 {
-	Random rnd=new Random();
-	for(int i=0; i<arr.GetLength(0); i++)
-	{
-		for(int j=0; j<arr.GetLength(1); j++)
-		{
-			arr[i,j]=rnd.Next(-9,10);
-		}
-	}
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(-9, 10);
+        }
+    }
 }
 
 void PrintArray(int[,] arr)
 {
-	
-	for(int i=0; i<arr.GetLength(0); i++)
-	{
-		for(int j=0; j<arr.GetLength(1); j++)
-		{
-			Console.Write($"{arr[i,j]}\t");
-		}
-		Console.WriteLine();
-	}
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
 }
 
 int[,] MultiplieArray(int[,] arr1, int[,] arr2)
 {
-	int[,] arr = new int[arr2.GetLength(0),arr1.GetLength(1)];
-	for(int i=0; i<arr.GetLength(0); i++)
-	{
-		for(int j=0; j<arr.GetLength(1); j++)
-		{
-			arr[i,j]=FindElMas(arr1, arr2, i, j);
-		}
-	}
-	return arr;
+    int[,] arr = new int[arr2.GetLength(0), arr1.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = FindElMas(arr1, arr2, i, j);
+        }
+    }
+    return arr;
 }
 
 int FindElMas(int[,] arr1, int[,] arr2, int pozM, int pozN)
 {
-	int sum=0;
-	for(int j=0; j<arr1.GetLength(1); j++)
-	{
-		sum=sum+(arr1[pozM,j]*arr2[j,pozN]);
-		
-	}
-return sum;
+    int sum = 0;
+    for (int j = 0; j < arr1.GetLength(1); j++)
+    {
+        sum = sum + (arr1[pozM, j] * arr2[j, pozN]);
+
+    }
+    return sum;
 }
---
+++ HomeWork8/Task4/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
+// 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
+
+int[,,] array = new int[2, 2, 2];
+FillingArray(array);
+Print(array);
+
+void FillingArray(int[,,] arr)
+{
+	Random rnd = new Random();
+	bool isUnik = false;
+	int newElArr = rnd.Next(10, 100);
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			for (int z = 0; z < arr.GetLength(2); z++)
+			{
+				while (!isUnik)
+				{
+					newElArr = rnd.Next(10, 100);
+					isUnik = IsUnic(arr, newElArr, i, j, z);
+				}
+
+				arr[i, j, z] = newElArr;
+				isUnik = false;
+			}
+		}
+	}
+}
+
+void Print(int[,,] arr)
+{
+
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int z = 0; z < arr.GetLength(2); z++)
+		{
+			for (int j = 0; j < arr.GetLength(1); j++)
+			{
+				Console.Write($"{arr[i, j, z]}({i},{j},{z})\t");
+			}
+		}
+		Console.WriteLine();
+	}
+}
+
+bool IsUnic(int[,,] arr, int el, int Pozi, int Pozj, int Pozz)
+{
+	bool isUnic = true;
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			for (int z = 0; z < arr.GetLength(2); z++)
+			{
+				if ((arr[i, j, z] == el))
+				{
+					isUnic = false;
+					break;
+				}
+			}
+		}
+	}
+	return isUnic;
+
+}
+
+
